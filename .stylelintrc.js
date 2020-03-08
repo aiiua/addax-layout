5e4127648fc2e2f89f93c09c94e64b978dea8d99
@Aiiua <347565645@qq.com>
2020-03-08 15:40:55 +0800

    chore: stylelint config

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 扩展检查配置
+    'extends': 'stylelint-config-standard',
+    //
+    'syntax': 'less',
+    //
+    root: true,
+    // 规则
+    rules: {
+        indentation: 4,
+    }
+}
