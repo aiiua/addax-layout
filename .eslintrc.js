f2b009afb5b8a3f6e3672bbc5da3d5a32d42c330
@Aiiua <347565645@qq.com>
2020-03-08 15:40:41 +0800

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    // 扩展
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    // 规则
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
+    },
+    // 解析设置
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
