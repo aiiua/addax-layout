f7f353afd531f39b89b7ece05d7d5c338448a7d2
@Aiiua <347565645@qq.com>
2020-03-08 15:42:02 +0800

    chore: babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+     // 预设
+    presets: [
+        ['@vue/app', {
+            useBuiltIns: 'entry',
+            polyfills: [
+                'es6.promise',
+                'es6.symbol'
+            ]
+        }]
+    ],
+    // 插件
+    plugins: [
+        [
+            'import',
+            {
+                libraryName: 'view-design',
+                libraryDirectory: 'src/components'
+            }
+        ]
+    ]
 }
