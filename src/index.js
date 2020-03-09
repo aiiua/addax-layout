487fb9a93093fe2caa1b86b81f56c612600a52a0
@Aiiua <347565645@qq.com>
2020-03-09 15:27:16 +0800

    feat: export components

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import AddaxHeader from './components/header'
+import AddaxLoading from './components/loading'
+import AddaxSider from './components/sider'
+
+export {
+    AddaxHeader,
+    AddaxLoading,
+    AddaxSider
+}
