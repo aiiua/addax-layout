3a933782cca6fcb234d5f0ad1f177cc71c12d2b8
@Aiiua <347565645@qq.com>
2020-03-09 17:47:13 +0800

    style: update index location

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import AddaxHeader from './components/header'
-import AddaxLoading from './components/loading'
-import AddaxSider from './components/sider'
-
-export {
-    AddaxHeader,
-    AddaxLoading,
-    AddaxSider
-}
